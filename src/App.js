d9a4a1bdc479c5ff933f03a2577ae5b7d183358d
The HarshCans <harshsaifai@yandex.com>
2026-06-02 00:04:11 +0530

    Refactor routing and component imports in App.js

--- src/App.js
+++ src/App.js
@@ -5,32 +5,43 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { auth } from "./firebase";
+
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Dashboard from "./components/students/Dashboard";
 import AdminDashboard from "./components/AdminDashboards";
 import TeacherDashboard from "./components/TestCreator/TeacherDashboard";
 import CreateTestMeta from "./components/TestCreator/CreateTestMeta";
-import TestMaking from "./components/TestCreator/TestMaking";
-import Happy from "./components/students/happy";
-import TestLibrary from "./components/students/TestLibrary";
-import "./components/CSS/index.css";
-import HomePage from "./Home";
-import ResultPage from "./components/students/Result";
 import TestView from "./components/students/TestView";
+import Happy from "./components/students/happy";
+import ResultPage from "./components/students/Result";
+import TestLibrary from "./components/students/TestLibrary";
 import AddQuestionForm from "./components/TestCreator/AddQuestionForm";
 import QuestionManager from "./components/TestCreator/QuestionManager";
+import HomePage from "./Home";
+
+import "./components/CSS/index.css";
 import "./components/CSS/media.css";
 
 function App() {
-
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/author/creation/new" element={<CreateTestMeta />} />
+          {/* Home */}
+          <Route path="/" element={<HomePage />} />
+
+          {/* Authentication */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+          {/* Dashboards */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/admin" element={<AdminDashboard />} />
           <Route path="/author/creation" element={<TeacherDashboard />} />
+
+          {/* Test Creation */}
+          <Route path="/author/creation/new" element={<CreateTestMeta />} />
           <Route
             path="/author/creation/:testID/manage"
             element={<QuestionManager />}
@@ -39,19 +50,17 @@ function App() {
             path="/author/creation/:testID/add-question"
             element={<AddQuestionForm />}
           />
+
+          {/* Student Test Routes */}
           <Route path="/testpanel/:testID" element={<TestView />} />
           <Route path="/exam/share/:testID" element={<Happy />} />
           <Route path="/result/:testID" element={<ResultPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/tests/library" element={<TestLibrary />} />
-        
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="*" element={<Navigate to="/" />} />
+
+          {/* Redirect unknown routes */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
-      {/* </div> */}
     </Router>
   );
 }
